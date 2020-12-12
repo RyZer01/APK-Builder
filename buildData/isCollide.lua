2b989d69ef3fd5cf028cb70e0c290526f0f0cc91
RyZer01 <73953792+RyZer01@users.noreply.github.com>
2020-12-12 07:13:51 +0200

    Delete isCollide.lua

--- buildData/isCollide.lua
+++
@@ -1,9 +0,0 @@
-DB = Object:extend()
-
-function DB:apply(x1, y1, x2, y2)
-    self.y1 = y1
-    self.y2 = y2
-    self.x1 = x1
-    self.x2 = x2
-    return math.sqrt( (self.y2 - self.y1)^2 + (self.x2 - self.x1)^2 )
-end
